f1e0270240efff5c39b6c27d7d697008e36d4804
Dmitry Moskowski <me@corpix.ru>
2018-04-28 22:55:10 +0000

    Fixed derivation

--- default.nix
+++ default.nix
@@ -5,7 +5,15 @@ buildGo19Package rec {
   name = "peephole-unstable-${version}";
   version = "development";
 
-  buildInputs = with pkgs; [ git glide ];
+  buildInputs = with pkgs; [ git dep ];
+
+  installPhase = ''
+    source $stdenv/setup
+    set -e
+
+    mkdir -p              $bin/bin
+    cp    go/bin/peephole $bin/bin
+  '';
 
   src = ./.;
   goPackagePath = "github.com/corpix/peephole";
